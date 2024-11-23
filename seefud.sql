aec1e0bcfd73b038b0bf61f8a659fd10040a8815
riskiputraalamzah <aryainggil3@gmail.com>
2024-11-23 13:50:13 +0700

    update seefud.sql

--- seefud.sql
+++ seefud.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 11, 2024 at 05:04 PM
+-- Generation Time: Nov 23, 2024 at 06:49 AM
 -- Server version: 8.0.30
--- PHP Version: 7.4.33
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,6 +39,33 @@ CREATE TABLE `feedback` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `ingredients`
+--
+
+CREATE TABLE `ingredients` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` longtext,
+  `qty` decimal(10,0) NOT NULL,
+  `unit` varchar(50) NOT NULL,
+  `image` longtext
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ingredients_product`
+--
+
+CREATE TABLE `ingredients_product` (
+  `id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `ingredient_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
@@ -48,8 +75,16 @@ CREATE TABLE `product` (
   `name` varchar(255) NOT NULL,
   `description` longtext NOT NULL,
   `price` decimal(10,0) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
   `qr_code` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `vendor_id`, `name`, `description`, `price`, `image`, `qr_code`) VALUES
+(1, 1, 'Product Example', 'Product description', 101, NULL, 'SampleQRCodeData');
 
 -- --------------------------------------------------------
 
@@ -61,10 +96,17 @@ CREATE TABLE `users` (
   `id` int NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('vendor','customer') CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `role` enum('vendor','customer','admin') CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `email` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `role`, `created_at`, `email`) VALUES
+(1, 'User Vendor', '$2b$10$a0JnB1kbFAF5qTf24Zj7auNd2twMHwTYJUiyrhzrMkD5Ki0kW4Rq.', 'vendor', '2024-11-23 05:31:13', 'vendor@example.com');
 
 -- --------------------------------------------------------
 
@@ -81,6 +123,13 @@ CREATE TABLE `vendor` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
+-- Dumping data for table `vendor`
+--
+
+INSERT INTO `vendor` (`id`, `user_id`, `store_name`, `description`, `location`) VALUES
+(1, 1, 'Store Example', 'A sample description', 'Sample Location');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -88,6 +137,18 @@ CREATE TABLE `vendor` (
 -- Indexes for table `feedback`
 --
 ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ingredients_product`
+--
+ALTER TABLE `ingredients_product`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -120,22 +181,34 @@ ALTER TABLE `feedback`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `ingredients`
+--
+ALTER TABLE `ingredients`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ingredients_product`
+--
+ALTER TABLE `ingredients_product`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `vendor`
 --
 ALTER TABLE `vendor`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
